cc0b7334af18e2f374595b20ee103554195f6e7a
Ben Harper <rogojin@gmail.com>
2026-01-25 12:03:29 +0200

    Add thread-safe EventStreamWriter
    Use this if you're writing events from multiple threads

--- eventstream.go
+++ eventstream.go
@@ -1,13 +1,20 @@
 package www
 
-import "net/http"
+import (
+	"net/http"
+	"sync"
+)
 
-// Send Content-Type:text/event-stream
+// Send Content-Type:text/event-stream.
+// Note: Use NewEventStreamWriter instead of StartEventStream if you're going to be
+// writing events from multiple threads.
 func StartEventStream(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "text/event-stream")
 }
 
-// WriteEvent writes a single event to the event stream
+// WriteEvent writes a single event to the event stream.
+// Note: This is not thread safe. Use EventStreamWriter if you're going to be writing
+// events from multiple threads.
 func WriteEvent(w http.ResponseWriter, event string, data string) error {
 	_, err := w.Write([]byte("event: " + event + "\n"))
 	if err != nil {
@@ -22,4 +29,23 @@ func WriteEvent(w http.ResponseWriter, event string, data string) error {
 		flusher.Flush()
 	}
 	return nil
+}
+
+// A thread-safe event stream writer
+type EventStreamWriter struct {
+	W    http.ResponseWriter
+	lock sync.Mutex
+}
+
+// Start an event stream, and return a thread-safe writer
+func NewEventStreamWriter(w http.ResponseWriter) *EventStreamWriter {
+	StartEventStream(w)
+	return &EventStreamWriter{W: w}
+}
+
+// WriteEvent writes a single event to the event stream in a thread-safe manner
+func (w *EventStreamWriter) WriteEvent(event string, data string) error {
+	w.lock.Lock()
+	defer w.lock.Unlock()
+	return WriteEvent(w.W, event, data)
 }
